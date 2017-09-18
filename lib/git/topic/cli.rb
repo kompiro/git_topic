ba5d7c77f0d14a3788bbfd40b287ea71d72d05e5
Hiroki Kondo <kompiro@gmail.com>
2017-09-18 15:49:57 +0900

    :sparkles: Add default command

--- lib/git/topic/cli.rb
+++ lib/git/topic/cli.rb
@@ -4,9 +4,15 @@ module Git
   module Topic
     # CLI command entry point
     class Cli < Thor
+      default_command :list
       desc 'hello NAME', 'say hello to NAME'
       def hello(name)
         puts "Hello #{name}"
+      end
+
+      desc 'list', 'show topics'
+      def list
+        puts 'list'
       end
     end
   end
