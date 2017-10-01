d7ab83f12eba0f11a41efa1c97da8a74557a4b26
Hiroki Kondo <kompiro@gmail.com>
2017-10-01 23:41:05 +0900

    :sparkles: Configure aggregate_failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
 end
 
 def setup_command(command, output)
